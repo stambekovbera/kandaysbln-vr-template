760e57d0ac040f6a67e100b8949e5a4104f3a040
Берадор Стамбеков <87119093+stambekovbera@users.noreply.github.com>
2024-11-30 02:59:14 +0500

    KVRT-1: Инициализация проекта (#1)

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import kandaysblnLogo from './shared/assets/kandaysbln-logo-white.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://github.com/stambekovbera" target="_blank">
+          <img src={kandaysblnLogo} className="logo react" alt="kandaysbln logo" />
+        </a>
+      </div>
+      <h1>React + TS + Vite</h1>
+    </>
+  )
+}
+
+export default App
