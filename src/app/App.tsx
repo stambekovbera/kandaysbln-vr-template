dfe1c188627d25025b8cc46b797ab2309dca94c8
Берадор Стамбеков <87119093+stambekovbera@users.noreply.github.com>
2024-11-30 14:03:01 +0500

    KVRT-4: Подключить SCSS (#2)

---
+++ src/app/App.tsx
@@ -0,0 +1,29 @@
+import kandaysblnLogo from '../shared/assets/kandaysbln-logo-white.svg'
+import './styles/index.scss'
+
+function App() {
+    return (
+        <>
+            <div className='top-content'>
+                <a href="https://github.com/stambekovbera" target="_blank">
+                    <img src={ kandaysblnLogo } className="logo react" alt="kandaysbln logo"/>
+                </a>
+                <h1 className='title'>React + TS + Vite</h1>
+            </div>
+            <div className='bottom-content'>
+                <h2 className='label'>Если этот шаблон оказался вам полезен и вам всё понравилось, пожалуйста, поставьте
+                    ⭐️ на <a className='link' target='_blank'
+                             href="https://github.com/stambekovbera/kandaysbln-vr-template">GitHub</a>.
+                    Это
+                    поможет другим разработчикам найти его!</h2>
+                <h2 className='label'>If you found this template helpful and enjoyed using it, please give it a ⭐️ on <a
+                    className='link'
+                    target='_blank'
+                    href="https://github.com/stambekovbera/kandaysbln-vr-template">GitHub</a>. It helps others
+                    discover it!</h2>
+            </div>
+        </>
+    )
+}
+
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig( {
+    plugins: [ react() ],
+    css: {
+        modules: {
+            localsConvention: "camelCaseOnly"
+        },
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler',
+                silenceDeprecations: ['import']
+            }
+        }
+    }
+} );
